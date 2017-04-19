fce955e033418464b6cb311e8d83cbf7de6177b8
한은이 <haneuneee@iMac.local>
2017-04-19 13:22:20 +0900

    Add Editor with Profile.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Editor from './Editor.js'
 
 class App extends Component {
+  constructor(){
+    super();
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e){
+    console.log(this,e);
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Stand Up 앱을 만들어 봅시다</h2>
+          <h2>만들어봅시당</h2>
         </div>
         <p className="App-intro">
           이제 여기서 부터 프로젝트는 시작입니다. 로고는 일단은 그냥 둡시다..
         </p>
+        <Editor {...this}/>
       </div>
     );
   }
---
+++ src/Editor.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './Editor.css';
+import Profile from './Profile.js'
+
+class Editor extends Component {
+
+  render() {
+    return (
+        <div className="wrapEditor">
+        <Profile isAnonymous={this.props.isAnonymous}/>
+        <div className="textEditor">
+        <div className="innerEdit"
+      contentEditable="true"
+      placeholder="글쓰기..."></div>
+        </div>
+        <div className="actionBar">
+        <button className="upload" onClick={this.props.handleSubmit}>
+        <span>제출</span>
+        </button>
+        </div>
+        </div>
+    )
+  }
+}
+
+export default Editor;
