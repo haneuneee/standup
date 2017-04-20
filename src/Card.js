44e76686bb614da133903dd6fefee51c532ce98d
한은이 <haneuneee@gmail.com>
2017-04-20 21:55:25 +0900

    Card Added and Article Moved.

---
+++ src/Card.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import './Card.css'
+export default class Card extends Component {
+  render(){
+    let cardInfo = this.props.cardInfo;
+    if(cardInfo){
+      return(
+        <a className="card" href={cardInfo.url} target="_blank">
+          <div className="card_image" >
+            <img src={cardInfo.thumbnailUrl} alt={cardInfo.title} className="width100 card_img"/>
+          </div>
+          <div className="borderTop">
+          <div className="card_text">
+            <p className="card_title">{cardInfo.title}</p>
+            <p className="card_desc">{cardInfo.description}</p>
+            <p className="card_provider">{cardInfo.url}</p>
+          </div>
+          </div>
+        </a>
+      )
+    }else{
+      return null;
+    }
+  }
+}
